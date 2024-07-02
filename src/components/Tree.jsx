0d8b8853c04dee3b64f44804745943f3bf25dceb
conaryHernandez <conaryh@gmail.com>
2024-07-01 19:49:44 -0600

    feat: Add FileExplorerTheme to Tree component

--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import SortableTree from "@nosferatu500/react-sortable-tree";
+import FileExplorerTheme from "@nosferatu500/theme-file-explorer";
 import "@nosferatu500/react-sortable-tree/style.css"; // This only needs to be imported once in your app
 
 export default class Tree extends Component {
@@ -21,6 +22,7 @@ export default class Tree extends Component {
           treeData={this.state.treeData}
           onChange={(treeData) => this.setState({ treeData })}
           isVirtualized={false}
+          theme={FileExplorerTheme}
         />
       </div>
     );
