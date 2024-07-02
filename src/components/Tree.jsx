f0f6e414aa6743e412a8cf3ad25ebb79440646f8
conaryHernandez <conaryh@gmail.com>
2024-07-01 19:43:58 -0600

    chore: Add react-sortable-tree dependency and implement tree component

---
+++ src/components/Tree.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import SortableTree from "@nosferatu500/react-sortable-tree";
+import "@nosferatu500/react-sortable-tree/style.css"; // This only needs to be imported once in your app
+
+export default class Tree extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      treeData: [
+        { title: "Chicken", children: [{ title: "Egg" }] },
+        { title: "Fish", children: [{ title: "fingerline" }] },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div style={{ height: 400 }}>
+        <SortableTree
+          treeData={this.state.treeData}
+          onChange={(treeData) => this.setState({ treeData })}
+          isVirtualized={false}
+        />
+      </div>
+    );
+  }
+}
